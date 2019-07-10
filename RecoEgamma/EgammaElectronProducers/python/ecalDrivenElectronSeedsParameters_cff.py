062d257ac2fecc556894e69881a256c6589d3a7c
Sam Harper <sam.j.harper@gmail.com>
2019-07-10 21:42:05 +0100

    adding light by light modifier

--- RecoEgamma/EgammaElectronProducers/python/ecalDrivenElectronSeedsParameters_cff.py
+++ RecoEgamma/EgammaElectronProducers/python/ecalDrivenElectronSeedsParameters_cff.py
@@ -87,3 +87,9 @@ ecalDrivenElectronSeedsParameters = cms.PSet(
 
 from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
 pp_on_AA_2018.toModify(ecalDrivenElectronSeedsParameters, SCEtCut = 15.0)
+
+
+from Configuration.Eras.Modifier_lightByLightLowPt_cff import lightByLightLowPt
+lightByLightLowPt.toModify(ecalDrivenElectronSeedsParameters,
+                           SCEtCut =1.0,
+                           applyHOverECut = False) 
